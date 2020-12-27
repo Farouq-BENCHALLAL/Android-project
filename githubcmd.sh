a10aa91b3c3fddb3a06a49477a60d2021ffa2f6b
farouq <farouq.benchallal@gmail.com>
2020-12-27 23:55:56 +0100

    charger mon projet sur github

---
+++ githubcmd.sh
@@ -0,0 +1,12 @@
+git init -b main
+
+git add .
+
+git commit -m "charger mon projet sur github"
+
+git remote add origin https://github.com/Farouq-BENCHALLAL/Android-project.git
+
+git remote -v
+
+git push origin main
+
